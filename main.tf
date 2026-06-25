7be5a75b32ded8e6fed656a69d14684737409196
sonu841438-beep <sonu841438@gmail.com>
2026-06-25 10:16:16 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+}
+resource "azurerm_network_interface" "sonunic987" {
+  for_each = var.nicsonu
+
+  name                = each.value.name
+  location            = each.value.location
+  resource_group_name = each.value.resource_group_name
+
+  ip_configuration {
+    name                          = each.value.ip_configuration.name
+    subnet_id                     = each.value.ip_configuration.subnet_id
+    private_ip_address_allocation = each.value.ip_configuration.private_ip_address_allocation
+  }
+}
